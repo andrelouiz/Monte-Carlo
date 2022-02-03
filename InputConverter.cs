1ddc3ee4c18ed77b64418af0b6280506ed4e7ad1
andrelouiz <andrelouiz@outlook.com>
2022-02-03 06:33:19 +0100

    New update

---
+++ InputConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monte_Carlo
+{
+    class InputConverter
+    {
+        public int[] Convert(string input)
+        {
+            if (Check(input))
+            {
+                string[] strArr = input.Replace(" ", string.Empty).Split(',');
+                int[] numbers = Array.ConvertAll(strArr, s => int.Parse(s));
+                return numbers;
+            }
+            return null;
+        }
+
+        public bool Check(string input)
+        {
+            string[] inpArr = input.Replace(" ", string.Empty).Split(',');
+            foreach (var str in inpArr)
+            {
+                if (!int.TryParse(str, out int temp) || inpArr.Length != 2)
+                {
+                    Console.WriteLine("Incorrect input");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
